fcc64a26c8a12aa8fb60d007de6995301cfb5612
lunalunaris <zuska77@wp.pl>
2022-11-19 11:52:58 +0100

    path

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -2,6 +2,7 @@
 import http.server
 import socketserver
 import os
+import urlparse
 from datetime import datetime
 #print('source code for "http.server":', http.server.__file__)
 
@@ -9,7 +10,7 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     
     def do_GET(self):
 
-        params = os.environ.get('QUERY_STRING')
+    
         if self.path == '/':
             
             self.protocol_version = 'HTTP/1.1'
